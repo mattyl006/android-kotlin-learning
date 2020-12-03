9c936aa2d37db00c743cc4e39be0d350ed8d1628
Mateusz <wirus006@gmail.com>
2020-12-03 13:59:15 +0100

    Implement not access to back to main view without set first password correctly

--- app/src/main/java/com/example/myapplication/SetFirstPass.kt
+++ app/src/main/java/com/example/myapplication/SetFirstPass.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SetFirstPass : AppCompatActivity() {
 
@@ -13,10 +16,29 @@ class SetFirstPass : AppCompatActivity() {
         setContentView(R.layout.activity_set_first_pass)
 
         val backButton = findViewById<Button>(R.id.btnBack);
+        val setPassword = findViewById<EditText>(R.id.txtPassword);
+        val savePasswordButton =  findViewById<Button>(R.id.btnSavePassword);
+
+        savePasswordButton.setOnClickListener(View.OnClickListener {
+            if(setPassword.text.toString().length < 16) {
+                Toast.makeText(this, "Typed password is too short, password minimal length is 16", Toast.LENGTH_LONG).show();
+            } else {
+                val sharedPreference = getSharedPreferences("passwordStorage", Context.MODE_PRIVATE);
+                val editor = sharedPreference.edit();
+                editor.putString("password", setPassword.text.toString());
+                editor.apply();
+            }
+        })
 
         backButton.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this, MainActivity::class.java);
-            startActivity(intent);
+            if(setPassword.text.toString() == "") {
+                Toast.makeText(this, "You have to set your first password, before go to main view!", Toast.LENGTH_LONG).show();
+                println("You have to set your first password, before go to main view!");
+
+            } else {
+                val intent = Intent(this, MainActivity::class.java);
+                startActivity(intent);
+            }
         })
     }
 }
